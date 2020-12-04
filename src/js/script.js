ae1694b62b03158fde0aaead2d51457fda54238d
Hera Gratton <sonicrock4ever@gmail.com>
2020-12-04 11:44:40 -0500

    Continuité JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     let connexion = new MovieDB;
-    connexion.requeteDernierFilm();
+
+    if(document.location.pathname.search('fiche-film.html') > 0) {
+        let params = new URL (document.location).searchParams;
+        console.log(params);
+        connexion.requeteInfoFilm(params.get('id'));
+    } else {
+        connexion.requeteDernierFilm();  
+    }
+
 });
 
 class MovieDB {
@@ -17,7 +25,7 @@ class MovieDB {
         let requete = new XMLHttpRequest();
         requete.addEventListener("loadend", this.retourDernierFilm.bind(this));
         //requete.open('GET', this.baseUrl + 'movie/now_playing?api_key='+ this.apiKey +'&language=' + this.lang + '&page=1');
-        requete.open('GET', this.baseUrl + 'movie/popular?api_key='+ this.apiKey +'&language=' + this.lang + '&page=1');
+        requete.open('GET', this.baseUrl + 'movie/now_playing?api_key='+ this.apiKey +'&language=' + this.lang + '&page=1');
         requete.send();
     }
 
@@ -49,10 +57,52 @@ class MovieDB {
 
             let image = article.querySelector('img');
             image.src = this.imgPath + "w300" + data[i].poster_path;
+            image.alt = data[i].title;
+
+            //Changer le href du lien a pour ajouter le id du film
+            article.querySelector('a').href = "fiche-film.html?id=" + data[i].id;
 
             //console.log(article);
 
             section.appendChild(article);
         }
+    }
+
+
+    requeteInfoFilm(movieId) {
+        let requete = new XMLHttpRequest();
+        requete.addEventListener("loadend", this.retourRequeteInfoFilm.bind(this));
+        //requete.open('GET', this.baseUrl + 'movie/now_playing?api_key='+ this.apiKey +'&language=' + this.lang + '&page=1');
+        requete.open('GET', this.baseUrl + 'movie/' + movieId + '?api_key=' + this.apiKey +'&language=' + this.lang);
+        //14?api_key=b22f9b20c68ad36893d3c8b75f29771a&language=en-US
+        requete.send();
+    }
+
+    retourRequeteInfoFilm(event) {
+        console.log('retourRequeteInfoFilm');
+        let target = event.currentTarget;
+        let data = JSON.parse(target.responseText);
+        console.log(data);
+
+        this.afficherInfoFilm(data);
+    }
+
+    afficherInfoFilm(data) {
+        console.log('afficherInfoFilm');
+        //this.requeteActeur();
+
+        document.querySelector('h1').innerHTML = data.title;
+    }
+
+    requeteActeur() {
+        //requete vers GET credit(movieDB)
+    }
+
+    retourRequeteActeur() {
+        //faire attention JSON.parse
+    }
+
+    afficheActeur() {
+        //Boucle for et clone de div.acteur
     }
 }
